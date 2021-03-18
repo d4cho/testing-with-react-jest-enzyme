df8604ce14dc5cfb683c287b7e0d78e86471f561
Daniel Cho <danielcho91@gmail.com>
2021-03-18 13:55:29 -0400

    Header Component and Tests

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,10 +5,10 @@ import Logo from "../../assets/images/logo.png";
 
 const Header = () => {
     return (
-        <header>
+        <header data-test="headerComponent">
             <div className="wrap">
                 <div className="logo">
-                    <img src={Logo} alt="logo" />
+                    <img data-test="logoIMG" src={Logo} alt="logo" />
                 </div>
             </div>
         </header>
